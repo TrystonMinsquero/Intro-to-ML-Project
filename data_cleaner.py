ce9b53865169fd34531e5f57739a0b87fc91d1f9
Tryston Minsquero <tam190005@utdallas.edu>
2022-05-04 12:20:41 -0500

    clean data

---
+++ data_cleaner.py
@@ -0,0 +1,54 @@
+import nltk
+from nltk.corpus import stopwords
+from textblob import Word
+import matplotlib.pyplot as plt
+import wordcloud
+import re
+
+# add sentiments based off the numerical rating on a dictionary
+def add_sentiments(data, cutoff=3.0):
+    for review in data:
+        sent = 'Positive' if review['rating'] > cutoff else 'Negative'
+        review['sentiment'] = sent
+
+#checks if there is any null values in the data
+def has_null(data_df):
+    data_v1 = data_df[['verified_reviews', 'sentiment']]
+    for row in data_v1.isnull().sum():
+        if row > 0:
+            return True
+    return False
+
+# needs to be ran once in order to clean data
+def download_nltk_requirements():
+    nltk.download('stopwords')
+    nltk.download('omw-1.4')
+
+# will clean the 'verified_reviews' column to be ready for analysis
+def clean(data_df):
+    stop_words = stopwords.words('english')
+
+    # To lowercase
+    data_df['verified_reviews'] = data_df['verified_reviews'].apply(lambda x: ' '.join(x.lower() for x in x.split()))
+
+    # Replacing the special characters and digits/numbers
+    data_df['verified_reviews'] = data_df['verified_reviews'].apply(lambda x: ' '.join(re.sub(r"[^a-zA-Z]", "", x) for x in x.split()))
+
+    # Removing stop words
+    data_df['verified_reviews'] = data_df['verified_reviews'].apply(lambda x: ' '.join(x for x in x.split() if x not in stop_words))
+
+    # Lemmatization
+    data_df['verified_reviews'] = data_df['verified_reviews'].apply(lambda x: ' '.join([Word(x).lemmatize() for x in x.split()]))
+
+    return data_df
+
+# returns a plt of the wordcloud of common words
+def get_common_wordcloud(data_df):
+    common_words=''
+    for i in data_df.verified_reviews:
+        common_words += ' '.join(str(i).split()) + ' '
+
+    word_cloud = wordcloud.WordCloud().generate(common_words)
+    plt.imshow(word_cloud, interpolation="bilinear")
+    plt.axis("off")
+    return plt
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+from asyncio.windows_events import NULL
 import streamlit as st
 from data_loader import data
-# Importing required libraries
-import nltk
+from data_cleaner import *
 import pandas as pd
-from nltk.corpus import stopwords
-from textblob import Word
+# Importing required libraries
 from sklearn.preprocessing import LabelEncoder
 from collections import Counter
-import wordcloud
 from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
 from sklearn.model_selection import train_test_split 
-import matplotlib.pyplot as plt
 
-st.write(data)
+# adds sentiment values based on 'verified reviews'
+add_sentiments(data)
+download_nltk_requirements()
+
+# convert data into a pandas dataframe
+data_df = pd.DataFrame(data)
+
+data_df = clean(data_df)
+
+
+st.pyplot(get_common_wordcloud(data_df))
+
+st.write(data_df)
